4dbb9746475579d735a53ad41fe1bae3930c3359
kiel <kielmitchell8@gmail.com>
2022-10-12 11:37:58 +0100

    bug fixes, adds comments

--- lab3.py
+++ lab3.py
@@ -1,37 +1,56 @@
+from inspect import stack
 import sqlite3
 import os
 from prints import printHeaderMultiple, printHeaderSingle
 
-#############-GLOBAL VARIABLE FOR TARGETING TABLES WITHIN DB#########################################################
-target_table = 0
+#############-GLOBAL VARIABLE FOR TARGETING TABLES WITHIN DB-#########################################################
+
+GLOBAL_TARGET = 0
+GLOBAL_TABLES=[]
+
 #####################################################################################################################
 
 class DBOperations:  
-  current_tables=[]
-  sql_create_table1 = "CREATE TABLE IF NOT EXISTS "     
-  sql_create_table2 = """ (
-     EmployeeID INTEGER PRIMARY KEY AUTOINCREMENT, Title VARCHAR(5), Forename VARCHAR(30), Surname VARCHAR(30), Email VARCHAR(80), SALARY INTEGER
-  );"""     
- 
+
   def __init__(self):
+    """
+    The constructor for DBOperations. 
+    Populates global variable GLOBAL_TABLES with the results of an SQL query,
+    Checks for pre-existing Employees table for initialisation message,
+    Uses SQL query to discern between creation or not of new Employees Table
+    """
+    global GLOBAL_TARGET, GLOBAL_TABLES
     try:
+      GLOBAL_TABLES = self.get_GLOBAL_TABLES()
       self.conn = sqlite3.connect("DBEmployees.db")
       self.cur = self.conn.cursor()
-      self.cur.execute(self.sql_create_table1+'Employees'+self.sql_create_table2)
-      self.current_tables = self.get_current_tables()
+      self.cur.execute('CREATE TABLE IF NOT EXISTS Employees (EmployeeID INTEGER PRIMARY KEY AUTOINCREMENT, Title VARCHAR(5), Forename VARCHAR(30), Surname VARCHAR(30), Email VARCHAR(80), SALARY INTEGER);')
       self.conn.commit()
+      if 'Employees' in GLOBAL_TABLES:
+        print("'Employees' table already exists\n")
+      else:
+        print("'Employees' table created upon initialisation\n")      
 
     except Exception as e:
+      print('hitting exception', e)
       return
-
     finally:
+      GLOBAL_TABLES = self.get_GLOBAL_TABLES()
       self.conn.close()
-
+ 
+#####################################################################################################################
+ 
   def get_connection(self):
     self.conn = sqlite3.connect("DBEmployees.db")
     self.cur = self.conn.cursor()
+     
+#####################################################################################################################
 
-  def get_current_tables(self):
+  def get_GLOBAL_TABLES(self):
+    """
+    Accesses the SQLite master table to retrieve names of all extant tables in database.
+    """
+    global GLOBAL_TABLES
     try:
       self.get_connection()
       self.cur.execute("SELECT name FROM sqlite_master WHERE type ='table' AND name NOT LIKE 'sqlite_%';")
@@ -39,65 +58,81 @@ class DBOperations:
       cleaned = []
       for result in results:
         cleaned.append(str(result).strip("('',)"))
-      self.current_tables = cleaned
+      GLOBAL_TABLES = cleaned
       return cleaned
 
     except Exception as e:
       print(e)
-
     finally: 
       self.conn.close()
-
+ 
+#####################################################################################################################
+ 
   def create_table(self):
+    """
+    Uses the same query as the init command, except for a user generated table name. 
+    The ability to create and target different tables was additional functionality deemed beneficial for the project.
+    """
+    global GLOBAL_TABLES
     try:
       os.system('cls' if os.name == 'nt' else 'clear')
       print ("*"*30+"\n")
       print("Current existing tables: ")
       count = 0
-      for table in self.current_tables:
+      for table in GLOBAL_TABLES:
         print(str(count) + ': ' +str(table))
         count +=1
-      new_table = input("Please enter the name of the table that you would like to CREATE: ")
-      
+      new_table = ''
+      while new_table == '' or new_table in GLOBAL_TABLES:
+        new_table = input("Please enter the name of the table that you would like to CREATE (Must be an original name): ")
       self.get_connection()
-      self.cur.execute(self.sql_create_table1+ new_table+ self.sql_create_table2)
-      self.current_tables.append(new_table)
+      self.cur.execute("CREATE TABLE IF NOT EXISTS " + new_table + " (EmployeeID INTEGER PRIMARY KEY AUTOINCREMENT, Title VARCHAR(5), Forename VARCHAR(30), Surname VARCHAR(30), Email VARCHAR(80), SALARY INTEGER);")
+      GLOBAL_TABLES.append(new_table)
       self.conn.commit()
       print("Table created successfully")
-      print(str(self.get_current_tables()))
+      print(str(self.get_GLOBAL_TABLES()))
       input("Press 'ENTER' to exit: ")
 
     except Exception as e:
       print(e)
       input("Press 'ENTER' to exit: ")
-    
     finally: 
       self.conn.close()
-
+ 
+#####################################################################################################################
+ 
   def select_table(self):
-    global target_table
+    """
+    Changes the global table-targeting variable. In all queries, table name is drawn from the list of table-names using this index. 
+    """
+    global GLOBAL_TARGET, GLOBAL_TABLES
     try:
       os.system('cls' if os.name == 'nt' else 'clear')
       print("Existing Tables: ")
       print ("*"*30)
       count = 0
-      for table in self.current_tables:
+      for table in GLOBAL_TABLES:
         print(str(count) + ': ' +str(table))
         count +=1
-      print ("*"*30 + "\n" + "Current table: " + str(self.current_tables[target_table]) + "\n" + "*"*30 + "\n")
+      print ("*"*30 + "\n" + "Current table: " + str(GLOBAL_TABLES[GLOBAL_TARGET]) + "\n" + "*"*30 + "\n")
       table_select = 999
-      while table_select not in range(len(self.current_tables)):
+      while table_select not in range(len(GLOBAL_TABLES)):
         table_select=int(input('Select target table number: '))
-      target_table=int(table_select)
-      print("Changed target table to: " + str(target_table) +": "+ str(self.current_tables[target_table]))
+      GLOBAL_TARGET=int(table_select)
+      print("Changed target table to: " + str(GLOBAL_TARGET) +": "+ str(GLOBAL_TABLES[GLOBAL_TARGET]))
       input("Press 'ENTER' to exit: ")
 
     except Exception as e:
       print(e)
       input("Press 'ENTER' to exit: ")
-
+ 
+#####################################################################################################################
+ 
   def insert_data(self):
-    global target_table
+    """
+    Adds a new Employee to the targeted table, which is chosen under the select table command
+    """
+    global GLOBAL_TARGET, GLOBAL_TABLES
     try:
       self.get_connection()
       os.system('cls' if os.name == 'nt' else 'clear')
@@ -107,7 +142,7 @@ class DBOperations:
       email = (str(input("Enter employee email: ")))
       salary = (str(input("Enter employee salary: ")))
       employee_list = [title,forename,surname,email,salary]    
-      table = self.current_tables[target_table]
+      table = GLOBAL_TABLES[GLOBAL_TARGET]
       self.cur.execute("INSERT INTO "+table+" (Title,Forename,Surname,Email,Salary) VALUES(" + ' "'+'" ,"'.join(employee_list) + '");')
       self.conn.commit()
       print("Inserted data successfully")
@@ -116,15 +151,19 @@ class DBOperations:
     except Exception as e:
       print(e)
       input("Press 'ENTER' to exit: ")
-    
     finally:
       self.conn.close()
-
+ 
+#####################################################################################################################
+ 
   def select_all(self):
-    global target_table
+    """
+    Displays all employee records in currently targeted table
+    """
+    global GLOBAL_TARGET, GLOBAL_TABLES
     try:
       self.get_connection()
-      res1 = self.current_tables[int(target_table)]
+      res1 = GLOBAL_TABLES[int(GLOBAL_TARGET)]
       print(res1)
       res = str(res1).strip("('',)")
       print(res)
@@ -134,13 +173,17 @@ class DBOperations:
 
     except Exception as e:
       print(e)
-    
     finally:
       input("Press 'ENTER' to exit: ")
       self.conn.close()
-    
+ 
+#####################################################################################################################
+     
   def search_data(self):
-    global target_table
+    """
+    Allows search queries on all employee fields, including salaray ranges rather than exact ammounts.
+    """
+    global GLOBAL_TARGET, GLOBAL_TABLES
     try:
       self.get_connection()
       os.system('cls' if os.name == 'nt' else 'clear')
@@ -157,7 +200,7 @@ class DBOperations:
       key = int(input("Enter Selection: "))
       searchTerm = terms[key]
       modifier = ' = '
-      table = self.current_tables[target_table]
+      table = GLOBAL_TABLES[GLOBAL_TARGET]
       os.system('cls' if os.name == 'nt' else 'clear')
       if int(key) == 6:
         mods = {1: " > ", 2: " >= ", 3: " = ", 4:" < ", 5:" <= "}
@@ -186,19 +229,24 @@ class DBOperations:
             
     except Exception as e:
       print(e)
-
     finally:
       self.conn.close()
-
+ 
+#####################################################################################################################
+ 
   def update_data(self):
-    global target_table
+    """
+    Uses conditional logic to determine the fields that should be adjusted, taking input for those fields when selected.
+    These are then appended to the SQL query string.
+    """
+    global GLOBAL_TARGET, GLOBAL_TABLES
     try:
       self.get_connection()
       os.system('cls' if os.name == 'nt' else 'clear')
       print ("*"*30 + "\n")
       print("Please enter the employee ID for the record that you would like to UPDATE : ")
       empID = int(input("Employee ID: "))
-      table = self.current_tables[target_table]     
+      table = GLOBAL_TABLES[GLOBAL_TARGET]     
       self.cur.execute("SELECT * FROM "+table+" WHERE EmployeeID="+str(empID)+";")
       result = self.cur.fetchone()
       if type(result)==type(tuple()):
@@ -236,7 +284,6 @@ class DBOperations:
         if boolE == 'y':
           terms['Salary']=str(input("Enter updated value: "))
     
-
         sqlStatement = "UPDATE "+str(table)+" SET "
         for key in terms:
           if terms[key]:
@@ -263,19 +310,24 @@ class DBOperations:
     except Exception as e:
       print(e)
       input("Press 'ENTER' to exit: ")
-
     finally:
       self.conn.close()
  
+#####################################################################################################################
+  
   def delete_data(self):
-    global target_table
+    """
+    Requires the user already know the EmployeeID before deletion, but does present the user with the record before final confirmation. 
+    Originally used a RETURNING statement to verify record deletion, but older versions of sqlite3 do not support this functionality, and the host development device does not support the newer versions.
+    """
+    global GLOBAL_TARGET, GLOBAL_TABLES
     try:
       self.get_connection()
       os.system('cls' if os.name == 'nt' else 'clear')
       print ("*"*30 + "\n")
       print("Please enter the employee ID for the record that you would like to DELETE : ")
       empID = int(input("Employee ID: "))
-      table = self.current_tables[target_table]     
+      table = GLOBAL_TABLES[GLOBAL_TARGET]     
       self.cur.execute("SELECT * FROM "+str(table)+" WHERE EmployeeID="+str(empID)+";")
       result = self.cur.fetchone()
       if type(result)==type(tuple()):
@@ -303,13 +355,19 @@ class DBOperations:
 
     finally: 
       self.conn.close()
-
+ 
+#####################################################################################################################
+ 
   def delete_tables(self):
+    """
+    Allows user to delete all or single table. If Employees table is deleted, it is immediately recreated upon returning to the main menu.
+    """
+    global GLOBAL_TARGET, GLOBAL_TABLES
     try:
       self.get_connection()
       os.system('cls' if os.name == 'nt' else 'clear')
       print ("*"*30 + "\n")
-      self.current_tables = self.get_current_tables()
+      GLOBAL_TABLES = self.get_GLOBAL_TABLES()
       print('1. Delete a single table')
       print('2. Delete all tables (reset the Database)')
       print('3. Exit')
@@ -320,26 +378,26 @@ class DBOperations:
         return
       elif select == '1':
         count = 0
-        for table in self.current_tables:
+        for table in GLOBAL_TABLES:
           print(str(count)+': '+ str(table).strip("('',)"))
           count+=1
         select = 999
-        while int(select) not in range(len(self.current_tables)):
+        while int(select) not in range(len(GLOBAL_TABLES)):
           select = input("Choose which table you would like to delete: ")
-        res1 = self.current_tables[int(select)]
+        res1 = GLOBAL_TABLES[int(select)]
         res = str(res1).strip("('',)")
         self.get_connection()
         self.cur.execute("DROP TABLE IF EXISTS "+res+';')
         self.conn.commit()
         print('Table: ' + res + " has been deleted.")
       elif select =='2':
- 
         count = 0
         answer=''
         while answer not in ['y','n']:
+         print('Current tables: '+str(GLOBAL_TABLES))
          answer =input("Do you wish to DELETE ALL TABLES? (Employees table will be re-initialised) (y/n): ")
         if answer == 'y':
-          for table in self.current_tables:
+          for table in GLOBAL_TABLES:
             self.get_connection()  
             self.cur.execute("DROP TABLE IF EXISTS "+ str(table).strip("('',)")+';')
             self.conn.commit()  
@@ -348,14 +406,22 @@ class DBOperations:
       print(e)
       
     finally:
+      GLOBAL_TABLES=self.get_GLOBAL_TABLES()
+      if len(GLOBAL_TABLES) >= GLOBAL_TARGET:
+        GLOBAL_TARGET = 0
       input("press 'ENTER' to exit." )
       self.conn.close()
-
+ 
+#####################################################################################################################
+ 
 while True:
   os.system('cls' if os.name == 'nt' else 'clear')
   db_ops = DBOperations()
-  result = db_ops.get_current_tables()
-  print("Current Table: " + str(result[target_table]))
+  result = db_ops.get_GLOBAL_TABLES()
+  try:
+    print("Current Table: " + str(result[GLOBAL_TARGET]))
+  except Exception as e:
+    print("Current Table: Employees")
 
   print ("\n Menu:")
   print ("*"*30 + "\n")
